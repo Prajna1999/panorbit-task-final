91f8dc3ce0562dfe1a5783c0f4530ee78aa90321
Prajna1999 <gituprajna20@gmail.com>
2023-07-11 16:58:44 +0530

    add search results view

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,11 +1,14 @@
 # panorbit-task-final-main/apps/users/urls.py
 
 from django.urls import path
-from .views import signup, login_view, validate_otp_view, logout_view
+from .views import signup, login_view, validate_otp_view, logout_view,search_view, search_results
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
     path('login/', login_view, name='login'),
     path('validate_otp/', validate_otp_view, name='validate_otp'),
     path('logout/', logout_view, name='logout'),
+    path('search/', search_view, name='search'),
+    path('search/results/',search_results, name="search_results")
+    
 ]
